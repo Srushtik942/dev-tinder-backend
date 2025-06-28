73dedcd8aa8e20e4d30bd1094586a9b73512f6a3
srushtikulkarni <srushtikulkarni09@gmail.com>
2025-06-28 11:26:16 +0530

    practicising route

--- src/app.js
+++ src/app.js
@@ -8,6 +8,20 @@ app.get('/',(req,res)=>{
     res.status(200).json("Hello, Srushti!! From the server!")
 })
 
+app.get('/hello/2',(req,res)=>{
+    res.status(200).json("Abra ka Dabra!")
+})
+
+app.post('/user',(req,res)=>{
+    console.log("Save data to the DB");
+    res.status(200).json("Saved data to db!")
+})
+
+app.delete('/user',(req,res)=>{
+    console.log("Deleted User Successfully!");
+    res.status(200).json("Delete")
+})
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000!")
